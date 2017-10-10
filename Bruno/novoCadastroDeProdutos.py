bbdcdb8b0656b06d5538cd20b7008d2ef4b01e0c
Unknown <wanderson.souza668@gmail.com>
2017-10-10 19:31:16 -0300

    Erro Carregamento dos Ícones e Imagens
    Foi corrigido o poblema de carregamento de imagens e ícones através do uso da variavel sys.path.

--- Bruno/novoCadastroDeProdutos.py
+++ Bruno/novoCadastroDeProdutos.py
@@ -1,20 +1,18 @@
-import os,sys
+import os, sys
 from PyQt4 import QtCore,QtGui
 
 import pymysql
 
-varBancoDeDados = 'LojaDB'
+varBancoDeDados = 'LojaDBBuno'
+varPath = sys.path[0] + '/'
 
 config ={'host':'localhost',
          'port':3306,
          'database':varBancoDeDados,
          'user':'root',
-         'password':'34387'}
+         'password':'12345'}
 db= pymysql.connect(** config)
 cursor = db.cursor()
-
-
-
   
 class ClasseAPP(QtGui.QWidget):
      
@@ -28,7 +26,7 @@ class ClasseAPP(QtGui.QWidget):
         self.setGeometry(300,100,0,0)
         self.setFixedSize(700,550)
         self.setWindowTitle("Produtos")
-        self.setWindowIcon(QtGui.QIcon('palet03.png'))
+        self.setWindowIcon(QtGui.QIcon(varPath + 'palet03.png'))
         
         self.home()
         
@@ -49,7 +47,7 @@ class ClasseAPP(QtGui.QWidget):
        
         self.pic = QtGui.QLabel(self)
        
-        self.pic.setPixmap(QtGui.QPixmap("palet03.png"))
+        self.pic.setPixmap(QtGui.QPixmap(varPath + "palet03.png"))
         self.pic.setGeometry(550, 5, 621,100)
         
 
@@ -104,7 +102,7 @@ class ClasseAPP(QtGui.QWidget):
         self.btnincluir.setGeometry(20,230, 106, 28)
         self.btnincluir.clicked.connect(self. InserirDados)
         self.btnincluir.clicked.connect(self.create_comsuta)
-        self.btnincluir.setIcon(QtGui.QIcon('add.png'))
+        self.btnincluir.setIcon(QtGui.QIcon(varPath + 'add.png'))
         
         
         
@@ -116,28 +114,28 @@ class ClasseAPP(QtGui.QWidget):
         self.btncomsuta.setGeometry(140, 230, 106, 28) 
         self.btncomsuta.clicked.connect(self.create_comsuta) 
          
-        self.btncomsuta.setIcon(QtGui.QIcon('repeat.png')) 
+        self.btncomsuta.setIcon(QtGui.QIcon(varPath + 'repeat.png')) 
 
         
         self.btnexcluir = QtGui.QPushButton("Excluir",self)
         self.btnexcluir.setGeometry(140, 485, 106, 28)
         self.btnexcluir.clicked.connect(self.EXcluir)
     
-        self.btnexcluir.setIcon(QtGui.QIcon('remove.png'))
+        self.btnexcluir.setIcon(QtGui.QIcon(varPath + 'remove.png'))
        
        
         self.btnatualizar = QtGui.QPushButton("Atualizar",self)
         self.btnatualizar.setGeometry(20,485, 106, 28)
         self.btnatualizar.clicked.connect(self. ATualizar)
         
-        self.btnatualizar.setIcon(QtGui.QIcon('up.png'))
+        self.btnatualizar.setIcon(QtGui.QIcon(varPath + 'up.png'))
        
         
         self.btnsair = QtGui.QPushButton("Sair",self)
         self.btnsair.setGeometry(260,485, 106, 28)
         self.btnsair.clicked.connect(self.sair)
         self.btnsair .clicked.connect(self. InserirDados)
-        self.btnsair .setIcon(QtGui.QIcon('accept.png'))
+        self.btnsair .setIcon(QtGui.QIcon(varPath + 'accept.png'))
        
 
 
@@ -416,7 +414,7 @@ class Excluir(QtGui.QDialog):
         self.setGeometry(450,260,0,0)
         self.setFixedSize(380,120)
         self.setWindowTitle("Excluir  Produtos")
-        self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+        self.setWindowIcon(QtGui.QIcon(varPath + 'pythonlogo.png'))
         
         self.home()
         
@@ -491,7 +489,7 @@ class Atualizar(QtGui.QDialog):
         self.setGeometry(325,260,0,0)
         self.setFixedSize(650,120)
         self.setWindowTitle("Produtos")
-        self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+        self.setWindowIcon(QtGui.QIcon(varPath + 'pythonlogo.png'))
         
         self.home()
         
@@ -506,7 +504,7 @@ class Atualizar(QtGui.QDialog):
         self.btnatualizar = QtGui.QPushButton('Atualizar',self)
         self.btnatualizar.setGeometry(520,45, 120, 28)
         self.btnatualizar.clicked.connect(self.Atualizar)
-        self.btnatualizar.setIcon(QtGui.QIcon('up.png'))
+        self.btnatualizar.setIcon(QtGui.QIcon(varPath + 'up.png'))
 
        
         
