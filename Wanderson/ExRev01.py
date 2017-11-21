3d44f3122a68d4624da9b730ec8bc925f6f4570a
Unknown <wanderson.souza668@gmail.com>
2017-11-21 21:14:47 -0200

    ExRev01 concluido
    Nome - Idade - Sexo

---
+++ Wanderson/ExRev01.py
@@ -0,0 +1,10 @@
+
+Nome = input ("Digite o seu nome:")
+Idade = input ("Digite a sua idade:")
+Sexo = input ("Digite o seu sexo:")
+NomedaMae = input ("Digite o nome da sua mãe:")
+
+print ("Seu nome é:",Nome)
+print ("sua idade é:",Idade)
+print ("seu sexo é:",Sexo)
+print ("O nome da sua mãe é:",NomedaMae)
