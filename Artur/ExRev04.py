4cfb9b59fa643dda045a2beaa0c53f68992012ff
Unknown <arturtutugnf@gmail.com>
2017-12-05 22:09:40 -0200

    ExRev4,5,6
    Fizemos os exercícios 4, 5 e 6.

---
+++ Artur/ExRev04.py
@@ -0,0 +1,18 @@
+media1 = float(input("Digite a media do primeiro bimestre:"))
+media2 = float(input("Digite a media do segundo bimestre:"))
+media3 = float(input("Digite a media do terceiro bimestre:"))
+media4 = float(input("Digite a media do segundo bimestre:"))
+
+media10 = media1 * 1
+media20 = media2 * 2
+media30 = media3 * 3
+media40 = media4 * 4
+
+mediasemifinal1 = media10 + media20
+mediasemifinal2 = media30 + media40
+
+mediafinal = mediasemifinal1 + mediasemifinal2
+
+mediaf = mediafinal / 10
+
+print("A sua media final e :",mediaf)
