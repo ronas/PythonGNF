b92a82968813179c6b5170eaf42154d142ff6674
Unknown <arturtutugnf@gmail.com>
2018-01-16 21:50:56 -0200

    Finalização LojaDB Produtos
    Possível finalização do programa LojaDb Produtos

--- Artur/LojaProdutos.py
+++ Artur/LojaProdutos.py
@@ -12,9 +12,6 @@ config = {
     'user' : 'root',
     'password' : ''
     }
-
-def sair():
-    sys.exit()
     
 class classApp(QtGui.QWidget):
     
@@ -23,9 +20,9 @@ class classApp(QtGui.QWidget):
         cursor = db.cursor()
         comando = ("delete from LojaDB.Produtos "
                    "where Codigo = '%s' ")
-        varcodigo = self.txtCodigoDeletar.text()
+        varcodigodelete = self.txtCodigoDeletar.text()
     
-        cursor.execute(comando,varcodigo)
+        cursor.execute(comando,varcodigodelete)
         db.commit()
         cursor.close()
         db.close()
@@ -40,123 +37,178 @@ class classApp(QtGui.QWidget):
             "values(%s,%s,%s,%s,%s,%s,%s,%s)"
             )
 #correto
-        varcodigo = self.txtCodigo.text()
-        varnome = self.txtNome.text()  
-        varunidademedida = self.txtUnidadeMedida.text()
-        varpeso = self.txtPeso.text()
-        varcodigoean = self.txtCodigoEAN.text()
-        varcodigomoeda = self.txtCodigoMoeda.text() 
-        varprecocompra = self.txtPrecoCompra.text()
-        varvalorvenda = self.txtValorVenda.text() 
-        dados = (varcodigo,
-                 varnome,
-                 varunidademedida,
-                 varpeso,
-                 varcodigoean,
-                 varcodigomoeda,
-                 varprecocompra,
-                 varvalorvenda)
+        varcodigoin = self.txtCodigoIn.text()
+        varnomein = self.txtNomeIn.text()  
+        varunidademedidain = self.txtUnidadeMedidaIn.text()
+        varpesoin = self.txtPesoIn.text()
+        varcodigoeanin = self.txtCodigoEANIn.text()
+        varcodigomoedain = self.txtCodigoMoedaIn.text() 
+        varprecocomprain = self.txtPrecoCompraIn.text()
+        varvalorvendain = self.txtValorVendaIn.text() 
+        dados = (varcodigoin,
+                 varnomein,
+                 varunidademedidain,
+                 varpesoin,
+                 varcodigoeanin,
+                 varcodigomoedain,
+                 varprecocomprain,
+                 varvalorvendain)
         cursor.execute(comando,dados)
         db.commit()
         cursor.close
         db.close
-        
-    def bdAtualizarNome(self):
+
+    def bdAtualizar(self):
         db = pymysql.connect(**config)
         cursor = db.cursor()
         comando = (
             "update LojaDB.Produtos "
-            "set Nome = %s where Codigo = %s "
+            "set Nome = %s,UnidadeMedida = %s,Peso = %s,CodigoEAN = %s,PrecoCompra = %s,ValorVenda = %s where Codigo = %s "
             )
         varcodigoat = self.txtCodigoAt.text()
         varnomeat = self.txtNomeAt.text() 
+        varunidademedidaat = self.txtUnidadeMedidaAt.text()
+        varpesoat = self.txtPesoAt.text()
+        varcodigoeanat = self.txtCodigoEANAt.text()
+        varcodigomoedaat = self.txtCodigoMoedaAt.text()
+        varprecocompraat = self.txtPrecoCompraAt.text()
+        varvalorvendaat = self.txtValorVendaAt.text()
 
-        dados =(varnomeat,varcodigoat)
+        dados =(varnomeat,varunidademedidaat,varpesoat,varcodigoeanat,varcodigomoedaat,varprecocompraat,varvalorvendaat,varcodigoat)
         cursor.execute(comando,dados)
         db.commit()
         cursor.close
         db.close    
+                 
+        
+#    def bdAtualizarNome(self):
+#        db = pymysql.connect(**config)
+#        cursor = db.cursor()
+#        comando = (
+#            "update LojaDB.Produtos "
+#            "set Nome = %s where Codigo = %s "
+#            )
+#        varcodigoat = self.txtCodigoAt.text()
+#        varnomeat = self.txtNomeAt.text() 
+#
+#        dados =(varnomeat,varcodigoat)
+#        cursor.execute(comando,dados)
+#        db.commit()
+#        cursor.close
+#        db.close    
          
-def bdAtualizarUnidadeMedida(self):
-        db = pymysql.connect(**config)
-        cursor = db.cursor()
-        comando = (
-            "update LojaDB.Produtos "
-            "set UnidadeMedida = %s where Codigo = %s "
-            )
-        varcodigoat = self.txtCodigoAt.text()
-        varunidademedidaat = self.txtUnidadeMedidaAt.text() 
+#def bdAtualizarUnidadeMedida(self):
+#        db = pymysql.connect(**config)
+#        cursor = db.cursor()
+#        comando = (
+#            "update LojaDB.Produtos "
+#            "set UnidadeMedida = %s where Codigo = %s "
+#            )
+#        varcodigoat = self.txtCodigoAt.text()
+#        varunidademedidaat = self.txtUnidadeMedidaAt.text() 
+#
+#        dados =(varunidaddemedidaat,varcodigoat)
+#        cursor.execute(comando,dados)
+#        db.commit()
+#        cursor.close
+#        db.close    
 
-        dados =(varunidaddemedidaat,varcodigoat)
-        cursor.execute(comando,dados)
-        db.commit()
-        cursor.close
-        db.close    
+#def bdAtualizarPeso(self):
+#        db = pymysql.connect(**config)
+#        cursor = db.cursor()
+#        comando = (
+#            "update LojaDB.Produtos "
+#            "set Peso = %s where Codigo = %s "
+#            )
+#        varcodigoat = self.txtCodigoAt.text()
+#        varpesoat = self.txtPesoAt.text() 
+#
+#        dados =(varpesoat,varcodigoat)
+#        cursor.execute(comando,dados)
+#        db.commit()
+#        cursor.close
+#        db.close    
 
-def bdAtualizarPeso(self):
-        db = pymysql.connect(**config)
-        cursor = db.cursor()
-        comando = (
-            "update LojaDB.Produtos "
-            "set Peso = %s where Codigo = %s "
-            )
-        varcodigoat = self.txtCodigoAt.text()
-        varpesoat = self.txtPesoAt.text() 
+#def bdAtualizarCodigoEAN(self):
+#        db = pymysql.connect(**config)
+#        cursor = db.cursor()
+#        comando = (
+#            "update LojaDB.Produtos "
+#            "set CodigoEAN = %s where Codigo = %s "
+#            )
+#        varcodigoat = self.txtCodigoAt.text()
+#        varcodigoeanat = self.txtCodigoEANAt.text() 
+#
+#        dados =(varcodigoeanat,varcodigoat)
+#        cursor.execute(comando,dados)
+#        db.commit()
+#        cursor.close
+#        db.close    
 
-        dados =(varpesoat,varcodigoat)
-        cursor.execute(comando,dados)
-        db.commit()
-        cursor.close
-        db.close    
+#def bdAtualizarCodigoMoeda(self):
+#        db = pymysql.connect(**config)
+#        cursor = db.cursor()
+#        comando = (
+#            "update LojaDB.Produtos "
+#            "set CodigoMoeda = %s where Codigo = %s "
+#            )
+#        varcodigoat = self.txtCodigoAt.text()
+#        varcodigomoedaat = self.txtCodigoMoedaAt.text() 
 
-def bdAtualizarCodigoEAN(self):
-        db = pymysql.connect(**config)
-        cursor = db.cursor()
-        comando = (
-            "update LojaDB.Produtos "
-            "set CodigoEAN = %s where Codigo = %s "
-            )
-        varcodigoat = self.txtCodigoAt.text()
-        varcodigoeanat = self.txtCodigoEANAt.text() 
+#        dados =(varcodigomoedaat,varcodigoat)
+#        cursor.execute(comando,dados)
+#        db.commit()
+#        cursor.close
+#        db.close    
 
-        dados =(varcodigoeanat,varcodigoat)
-        cursor.execute(comando,dados)
-        db.commit()
-        cursor.close
-        db.close    
+#def bdAtualizarPrecoCompra(self):
+#        db = pymysql.connect(**config)
+#        cursor = db.cursor()
+#        comando = (
+#            "update LojaDB.Produtos "
+#            "set PrecoCompra = %s where Codigo = %s "
+#            )
+#        varcodigoat = self.txtCodigoAt.text()
+#        varprecocompraat = self.txtPrecoCompraAt.text() 
+#
+#        dados =(varprecocompraat,varcodigoat)
+#        cursor.execute(comando,dados)
+#        db.commit()
+#        cursor.close
+#        db.close    
 
-def bdAtualizarCodigoMoeda(self):
-        db = pymysql.connect(**config)
-        cursor = db.cursor()
-        comando = (
-            "update LojaDB.Produtos "
-            "set CodigoMoeda = %s where Codigo = %s "
-            )
-        varcodigoat = self.txtCodigoAt.text()
-        varcodigomoedaat = self.txtCodigoMoedaAt.text() 
+#def bdAtualizarValorVenda(self):
+#        db = pymysql.connect(**config)
+#        cursor = db.cursor()
+#        comando = (
+#            "update LojaDB.Produtos "
+#            "set ValorVenda = %s where Codigo = %s "
+#            )
+#        varcodigoat = self.txtCodigoAt.text()
+#        varvalorvendaat = self.txtValorVendaAt.text() 
 
-        dados =(varcodigomoedaat,varcodigoat)
-        cursor.execute(comando,dados)
-        db.commit()
-        cursor.close
-        db.close    
-#Correto
-__________________________________________________________________________________________________________________  
+#        dados =(varvalorvendaat,varcodigoat)
+#        cursor.execute(comando,dados)
+#        db.commit()
+#        cursor.close
+#        db.close    
+
+  
     def bdMostrarProdutos(self):
         db = pymysql.connect(**config)
         cursor = db.cursor()
-        comando = 'select * from Produtos'
+        comando = 'select * from produtos'
         cursor.execute(comando)
         registros = cursor.fetchall()
         for registro in registros:
-            self.txtCodigo.setText(registro[0])
-            self.txtNome.setText(registro[1])
-            self.txtUnidadeMedida.setText(registro[2])
-            self.txtPeso.setText(str(registro[3]))
-            self.txtCodigoEAN.setText(registro[4])
-            self.txtCodigoMoeda.setText(registro[5])
-            self.txtPrecoCompra.setText(str(registro[6]))
-            self.txtValorVenda.setText(str(registro[7]))
+            self.txtCodigoM.setText(registro[0])
+            self.txtNomeM.setText(registro[1])
+            self.txtUnidadeMedidaM.setText(registro[2])
+            self.txtPesoM.setText(str(registro[3]))
+            self.txtCodigoEANM.setText(registro[4])
+            self.txtCodigoMoedaM.setText(registro[5])
+            self.txtPrecoCompraM.setText(str(registro[6]))
+            self.txtValorVendaM.setText(str(registro[7]))
             
         cursor.close
         db.close 
@@ -178,22 +230,38 @@ ________________________________________________________________________________
         self.lblCodigoMoeda = QtGui.QLabel('CodigoMoeda')
         self.lblPrecoCompra = QtGui.QLabel('PrecoCompra')
         self.lblValorVenda = QtGui.QLabel('ValorVenda')
-        self.lblCodigoDeletar = QtGui.QLabel('CodigoExcluir')
-        self.lblCodigoAtualizar = QtGui.QLabel('CodigoAtualizar')
-        self.lblPrecoCompraAtualizar = QtGui.QLabel('PrecoCompra')
+
         
-        self.txtCodigo = QtGui.QLineEdit()
-        self.txtNome = QtGui.QLineEdit()
-        self.txtUnidadeMedida = QtGui.QLineEdit()
-        self.txtPeso = QtGui.QLineEdit()
-        self.txtCodigoEAN = QtGui.QLineEdit()
-        self.txtCodigoMoeda = QtGui.QLineEdit()
-        self.txtPrecoCompra = QtGui.QLineEdit()
-        self.txtValorVenda = QtGui.QLineEdit()
+        
+        self.txtCodigoIn = QtGui.QLineEdit()
+        self.txtNomeIn = QtGui.QLineEdit()
+        self.txtUnidadeMedidaIn = QtGui.QLineEdit()
+        self.txtPesoIn = QtGui.QLineEdit()
+        self.txtCodigoEANIn = QtGui.QLineEdit()
+        self.txtCodigoMoedaIn = QtGui.QLineEdit()
+        self.txtPrecoCompraIn = QtGui.QLineEdit()
+        self.txtValorVendaIn = QtGui.QLineEdit()
+
+        self.txtCodigoAt = QtGui.QLineEdit()
+        self.txtNomeAt = QtGui.QLineEdit()
+        self.txtUnidadeMedidaAt = QtGui.QLineEdit()        
+        self.txtPesoAt = QtGui.QLineEdit()
+        self.txtCodigoEANAt = QtGui.QLineEdit()
+        self.txtCodigoMoedaAt = QtGui.QLineEdit()
+        self.txtPrecoCompraAt = QtGui.QLineEdit()
+        self.txtValorVendaAt = QtGui.QLineEdit()
+
+        self.txtCodigoM = QtGui.QLineEdit()
+        self.txtNomeM = QtGui.QLineEdit()
+        self.txtUnidadeMedidaM = QtGui.QLineEdit()
+        self.txtPesoM = QtGui.QLineEdit()
+        self.txtCodigoEANM = QtGui.QLineEdit()
+        self.txtCodigoMoedaM = QtGui.QLineEdit()
+        self.txtPrecoCompraM = QtGui.QLineEdit()
+        self.txtValorVendaM = QtGui.QLineEdit()
+
         self.txtCodigoDeletar = QtGui.QLineEdit()
-        self.txtCodigoAtualizar = QtGui.QLineEdit()
-        self.txtPrecoCompraAtualizar = QtGui.QLineEdit()
-        
+
         self.btnSair = QtGui.QPushButton('Sair',self)
         self.btnSair.clicked.connect(self.sair)
         self.btnBuscar = QtGui.QPushButton('Buscar',self)
@@ -203,39 +271,67 @@ ________________________________________________________________________________
         self.btnDeletar = QtGui.QPushButton('Deletar',self)
         self.btnDeletar.clicked.connect(self.bdExcluirProdutos)
         self.btnAtualizar = QtGui.QPushButton('Atualizar',self)
-        self.btnAtualizar.clicked.connect(self.bdAtualizarProdutos)
+        self.btnAtualizar.clicked.connect(self.bdAtualizar)
         
         self.grid = QtGui.QGridLayout()
         self.grid.setSpacing(10)         
 # Codigo  Nome  UnidadeMedida  Peso  CodigoEAN CodigoMoeda  PrecoCompra  ValorVenda        
-        self.grid.addWidget(self.lblCodigo,1,0)
-        self.grid.addWidget(self.txtCodigo,1,1)
-        self.grid.addWidget(self.lblNome,2,0)
-        self.grid.addWidget(self.txtNome,2,1)
-        self.grid.addWidget(self.lblUnidadeMedida,3,0)
-        self.grid.addWidget(self.txtUnidadeMedida,3,1)       
-        self.grid.addWidget(self.lblPeso,4,0)
-        self.grid.addWidget(self.txtPeso,4,1)
-        self.grid.addWidget(self.lblCodigoEAN,5,0)
-        self.grid.addWidget(self.txtCodigoEAN,5,1)
-        self.grid.addWidget(self.lblPrecoCompra,6,0)
-        self.grid.addWidget(self.txtPrecoCompra,6,1)
-        self.grid.addWidget(self.lblValorVenda,7,0)
-        self.grid.addWidget(self.txtValorVenda,7,1)
-        
-        self.grid.addWidget(self.btnSair,9,1)
-        self.grid.addWidget(self.btnBuscar,9,0)
-        self.grid.addWidget(self.btnInserir,9,2)
-        self.grid.addWidget(self.lblCodigoDeletar,8,0)
-        self.grid.addWidget(self.txtCodigoDeletar,8,1)
-        self.grid.addWidget(self.btnDeletar,9,3)
-        self.grid.addWidget(self.btnAtualizar,9,4)
-        
+        self.grid.addWidget(self.btnBuscar,1,1)
+        self.grid.addWidget(self.btnInserir,1,2)
+        self.grid.addWidget(self.btnAtualizar,1,3)
+
+
+        self.grid.addWidget(self.lblCodigo,2,0)
+        self.grid.addWidget(self.txtCodigoM,2,1)
+        self.grid.addWidget(self.txtCodigoIn,2,3)
+        self.grid.addWidget(self.txtCodigoAt,2,4)
+
+        self.grid.addWidget(self.lblNome,3,0)
+        self.grid.addWidget(self.txtNomeM,3,1)
+        self.grid.addWidget(self.txtNomeIn,3,2)
+        self.grid.addWidget(self.txtNomeAt,3,3)
+
+        self.grid.addWidget(self.lblUnidadeMedida,4,0)
+        self.grid.addWidget(self.txtUnidadeMedidaM,4,1)
+        self.grid.addWidget(self.txtUnidadeMedidaIn,4,2)
+        self.grid.addWidget(self.txtUnidadeMedidaAt,4,3)
+
+        self.grid.addWidget(self.lblPeso,5,0)
+        self.grid.addWidget(self.txtPesoM,5,1)
+        self.grid.addWidget(self.txtPesoIn,5,2)
+        self.grid.addWidget(self.txtPesoAt,5,3)
+
+        self.grid.addWidget(self.lblCodigoEAN,6,0)
+        self.grid.addWidget(self.txtCodigoEANM,6,1)
+        self.grid.addWidget(self.txtCodigoEANIn,6,2)
+        self.grid.addWidget(self.txtCodigoEANAt,6,3)
+
+        self.grid.addWidget(self.lblCodigoMoeda,7,0)
+        self.grid.addWidget(self.txtCodigoMoedaM,7,1)
+        self.grid.addWidget(self.txtCodigoMoedaIn,7,2)
+        self.grid.addWidget(self.txtCodigoMoedaAt,7,3)
+
+        self.grid.addWidget(self.lblPrecoCompra,8,0)
+        self.grid.addWidget(self.txtPrecoCompraM,8,1)
+        self.grid.addWidget(self.txtPrecoCompraIn,8,2)
+        self.grid.addWidget(self.txtPrecoCompraAt,8,3)
+
+        self.grid.addWidget(self.lblValorVenda,9,0)
+        self.grid.addWidget(self.txtValorVendaM,9,1)
+        self.grid.addWidget(self.txtValorVendaIn,9,2)
+        self.grid.addWidget(self.txtValorVendaAt,9,3)
+
+        self.grid.addWidget(self.txtCodigoDeletar,10,1)
+
+        self.grid.addWidget(self.btnDeletar,10,0)
+
+        self.grid.addWidget(self.btnSair,10,2)
+ 
         self.setLayout(self.grid)
         
         self.show()
     def sair(self):
-        sys.exit 
+        sys.exit()
         
 def main():
     app = QtGui.QApplication(sys.argv)
