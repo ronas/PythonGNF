4d924188f995ae19062f32c160615d0673db80f8
Unknown <arturtutugnf@gmail.com>
2018-03-06 22:06:34 -0300

    LojaDBParceiros
    Houve a finalização de uma possível versão final do programa LojaDBParceiros.

--- Artur/LojadbParceiros.py
+++ Artur/LojadbParceiros.py
@@ -18,7 +18,7 @@ class classApp(QtGui.QWidget):
     def bdExcluirParceiros(self):
         db = pymysql.connect(**config)
         cursor = db.cursor()
-        comando = ("delete from LojaDB.Parceiros "
+        comando = ("delete from LojaDB.parceiros "
                    "where Codigo = (%s) ")
         varcodigodelete = self.txtCodigoDeletar.text()
     
@@ -34,7 +34,7 @@ class classApp(QtGui.QWidget):
         cursor = db.cursor()
         comando =(
             "insert into Parceiros (Codigo,Razao,CNPJ,Endereco,Bairro,CEP,Cidade,Estado,Pais,Contato,Telefone,Email,LimiteDeCredito,AprovadorFinanceiro,Bloqueado)"
-            "values(%s,%s,%s,%s,%s,%s,%s,%s)"
+            "values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
             )
 #Codigo,Razao,CNPJ,Endereco,Bairro,CEP,Cidade,Estado,Pais,Contato,Telefone,Email,LimiteDeCredito,AprovadorFinanceiro,Bloqueado
         varcodigoin = self.txtCodigoIn.text()
@@ -71,7 +71,7 @@ class classApp(QtGui.QWidget):
         cursor.close
         db.close
 
-    def bdAtualizar(self):
+    def bdAtualizarParceiros(self):
         db = pymysql.connect(**config)
         cursor = db.cursor()
         comando = (
@@ -101,7 +101,7 @@ class classApp(QtGui.QWidget):
         db.close    
                  
         
-    def bdMostrarProdutos(self):
+    def bdMostrarParceiros(self):
 
         db = pymysql.connect(**config)
         cursor = db.cursor()
@@ -157,7 +157,7 @@ class classApp(QtGui.QWidget):
         self.lblEmail = QtGui.QLabel('Email')
         self.lblLimiteDeCredito = QtGui.QLabel('LimiteDeCredito')
         self.lblAprovadorFinanceiro = QtGui.QLabel('AprovadorFinanceiro')
-        self.lblBloqueado = QtGui.QLabel(Bloqueado)
+        self.lblBloqueado = QtGui.QLabel('Bloqueado')
 
         self.txtCodigoIn = QtGui.QLineEdit()
         self.txtRazaoIn = QtGui.QLineEdit()
@@ -176,39 +176,53 @@ class classApp(QtGui.QWidget):
         self.txtBloqueadoIn = QtGui.QLineEdit()
 #____________________________________________________________________________________________________________________________________________
         self.txtCodigoAt = QtGui.QLineEdit()
-        self.txtNomeAt = QtGui.QLineEdit()
-        self.txtUnidadeMedidaAt = QtGui.QLineEdit()        
-        self.txtPesoAt = QtGui.QLineEdit()
-        self.txtCodigoEANAt = QtGui.QLineEdit()
-        self.txtCodigoMoedaAt = QtGui.QLineEdit()
-        self.txtPrecoCompraAt = QtGui.QLineEdit()
-        self.txtValorVendaAt = QtGui.QLineEdit()
+        self.txtRazaoAt = QtGui.QLineEdit()
+        self.txtCNPJAt = QtGui.QLineEdit()        
+        self.txtEnderecoAt = QtGui.QLineEdit()
+        self.txtBairroAt = QtGui.QLineEdit()
+        self.txtCEPAt = QtGui.QLineEdit()
+        self.txtCidadeAt = QtGui.QLineEdit()
+        self.txtEstadoAt = QtGui.QLineEdit()
+        self.txtPaisAt = QtGui.QLineEdit()
+        self.txtContatoAt = QtGui.QLineEdit()
+        self.txtTelefoneAt = QtGui.QLineEdit()
+        self.txtEmailAt = QtGui.QLineEdit()
+        self.txtLimiteDeCreditoAt = QtGui.QLineEdit()
+        self.txtAprovadorFinanceiroAt = QtGui.QLineEdit()
+        self.txtBloqueadoAt = QtGui.QLineEdit()
 
         self.txtCodigoM = QtGui.QLineEdit()
-        self.txtNomeM = QtGui.QLineEdit()
-        self.txtUnidadeMedidaM = QtGui.QLineEdit()
-        self.txtPesoM = QtGui.QLineEdit()
-        self.txtCodigoEANM = QtGui.QLineEdit()
-        self.txtCodigoMoedaM = QtGui.QLineEdit()
-        self.txtPrecoCompraM = QtGui.QLineEdit()
-        self.txtValorVendaM = QtGui.QLineEdit()
+        self.txtRazaoM = QtGui.QLineEdit()
+        self.txtCNPJM = QtGui.QLineEdit()
+        self.txtEnderecoM = QtGui.QLineEdit()
+        self.txtBairroM = QtGui.QLineEdit()
+        self.txtCEPM = QtGui.QLineEdit()
+        self.txtCidadeM = QtGui.QLineEdit()
+        self.txtEstadoM = QtGui.QLineEdit()
+        self.txtPaisM = QtGui.QLineEdit()
+        self.txtContatoM = QtGui.QLineEdit()
+        self.txtTelefoneM = QtGui.QLineEdit()
+        self.txtEmailM = QtGui.QLineEdit()
+        self.txtLimiteDeCreditoM = QtGui.QLineEdit()
+        self.txtAprovadorFinanceiroM = QtGui.QLineEdit()
+        self.txtBloqueadoM = QtGui.QLineEdit()
 
         self.txtCodigoDeletar = QtGui.QLineEdit()
 
         self.btnSair = QtGui.QPushButton('Sair',self)
         self.btnSair.clicked.connect(self.sair)
         self.btnBuscar = QtGui.QPushButton('Buscar',self)
-        self.btnBuscar.clicked.connect(self.bdMostrarProdutos)
+        self.btnBuscar.clicked.connect(self.bdMostrarParceiros)
         self.btnInserir = QtGui.QPushButton('Inserir',self)
-        self.btnInserir.clicked.connect(self.bdInserirProdutos)
+        self.btnInserir.clicked.connect(self.bdInserirParceiros)
         self.btnDeletar = QtGui.QPushButton('Deletar',self)
-        self.btnDeletar.clicked.connect(self.bdExcluirProdutos)
+        self.btnDeletar.clicked.connect(self.bdExcluirParceiros)
         self.btnAtualizar = QtGui.QPushButton('Atualizar',self)
-        self.btnAtualizar.clicked.connect(self.bdAtualizar)
+        self.btnAtualizar.clicked.connect(self.bdAtualizarParceiros)
         
         self.grid = QtGui.QGridLayout()
         self.grid.setSpacing(10)         
-# Codigo  Nome  UnidadeMedida  Peso  CodigoEAN CodigoMoeda  PrecoCompra  ValorVenda        
+        
         self.grid.addWidget(self.btnBuscar,1,1)
         self.grid.addWidget(self.btnInserir,1,2)
         self.grid.addWidget(self.btnAtualizar,1,3)
@@ -219,46 +233,81 @@ class classApp(QtGui.QWidget):
         self.grid.addWidget(self.txtCodigoIn,2,2)
         self.grid.addWidget(self.txtCodigoAt,2,3)
 
-        self.grid.addWidget(self.lblNome,3,0)
-        self.grid.addWidget(self.txtNomeM,3,1)
-        self.grid.addWidget(self.txtNomeIn,3,2)
-        self.grid.addWidget(self.txtNomeAt,3,3)
+        self.grid.addWidget(self.lblRazao,3,0)
+        self.grid.addWidget(self.txtRazaoM,3,1)
+        self.grid.addWidget(self.txtRazaoIn,3,2)
+        self.grid.addWidget(self.txtRazaoAt,3,3)
 
-        self.grid.addWidget(self.lblUnidadeMedida,4,0)
-        self.grid.addWidget(self.txtUnidadeMedidaM,4,1)
-        self.grid.addWidget(self.txtUnidadeMedidaIn,4,2)
-        self.grid.addWidget(self.txtUnidadeMedidaAt,4,3)
+        self.grid.addWidget(self.lblCNPJ,4,0)
+        self.grid.addWidget(self.txtCNPJM,4,1)
+        self.grid.addWidget(self.txtCNPJIn,4,2)
+        self.grid.addWidget(self.txtCNPJAt,4,3)
 
-        self.grid.addWidget(self.lblPeso,5,0)
-        self.grid.addWidget(self.txtPesoM,5,1)
-        self.grid.addWidget(self.txtPesoIn,5,2)
-        self.grid.addWidget(self.txtPesoAt,5,3)
+        self.grid.addWidget(self.lblEndereco,5,0)
+        self.grid.addWidget(self.txtEnderecoM,5,1)
+        self.grid.addWidget(self.txtEnderecoIn,5,2)
+        self.grid.addWidget(self.txtEnderecoAt,5,3)
 
-        self.grid.addWidget(self.lblCodigoEAN,6,0)
-        self.grid.addWidget(self.txtCodigoEANM,6,1)
-        self.grid.addWidget(self.txtCodigoEANIn,6,2)
-        self.grid.addWidget(self.txtCodigoEANAt,6,3)
+        self.grid.addWidget(self.lblBairro,6,0)
+        self.grid.addWidget(self.txtBairroM,6,1)
+        self.grid.addWidget(self.txtBairroIn,6,2)
+        self.grid.addWidget(self.txtBairroAt,6,3)
 
-        self.grid.addWidget(self.lblCodigoMoeda,7,0)
-        self.grid.addWidget(self.txtCodigoMoedaM,7,1)
-        self.grid.addWidget(self.txtCodigoMoedaIn,7,2)
-        self.grid.addWidget(self.txtCodigoMoedaAt,7,3)
+        self.grid.addWidget(self.lblCEP,7,0)
+        self.grid.addWidget(self.txtCEPM,7,1)
+        self.grid.addWidget(self.txtCEPIn,7,2)
+        self.grid.addWidget(self.txtCEPAt,7,3)
 
-        self.grid.addWidget(self.lblPrecoCompra,8,0)
-        self.grid.addWidget(self.txtPrecoCompraM,8,1)
-        self.grid.addWidget(self.txtPrecoCompraIn,8,2)
-        self.grid.addWidget(self.txtPrecoCompraAt,8,3)
+        self.grid.addWidget(self.lblCidade,8,0)
+        self.grid.addWidget(self.txtCidadeM,8,1)
+        self.grid.addWidget(self.txtCidadeIn,8,2)
+        self.grid.addWidget(self.txtCidadeAt,8,3)
 
-        self.grid.addWidget(self.lblValorVenda,9,0)
-        self.grid.addWidget(self.txtValorVendaM,9,1)
-        self.grid.addWidget(self.txtValorVendaIn,9,2)
-        self.grid.addWidget(self.txtValorVendaAt,9,3)
+        self.grid.addWidget(self.lblEstado,9,0)
+        self.grid.addWidget(self.txtEstadoM,9,1)
+        self.grid.addWidget(self.txtEstadoIn,9,2)
+        self.grid.addWidget(self.txtEstadoAt,9,3)
 
-        self.grid.addWidget(self.txtCodigoDeletar,10,1)
+        self.grid.addWidget(self.lblPais,10,0)
+        self.grid.addWidget(self.txtPaisM,10,1)
+        self.grid.addWidget(self.txtPaisIn,10,2)
+        self.grid.addWidget(self.txtPaisAt,10,3)
 
-        self.grid.addWidget(self.btnDeletar,10,0)
+        self.grid.addWidget(self.lblContato,11,0)
+        self.grid.addWidget(self.txtContatoM,11,1)
+        self.grid.addWidget(self.txtContatoIn,11,2)
+        self.grid.addWidget(self.txtContatoAt,11,3)
 
-        self.grid.addWidget(self.btnSair,10,2)
+        self.grid.addWidget(self.lblTelefone,12,0)
+        self.grid.addWidget(self.txtTelefoneM,12,1)
+        self.grid.addWidget(self.txtTelefoneIn,12,2)
+        self.grid.addWidget(self.txtTelefoneAt,12,3)
+
+        self.grid.addWidget(self.lblEmail,13,0)
+        self.grid.addWidget(self.txtEmailM,13,1)
+        self.grid.addWidget(self.txtEmailIn,13,2)
+        self.grid.addWidget(self.txtEmailAt,13,3)
+
+        self.grid.addWidget(self.lblLimiteDeCredito,14,0)
+        self.grid.addWidget(self.txtLimiteDeCreditoM,14,1)
+        self.grid.addWidget(self.txtLimiteDeCreditoIn,14,2)
+        self.grid.addWidget(self.txtLimiteDeCreditoAt,14,3)
+
+        self.grid.addWidget(self.lblAprovadorFinanceiro,15,0)
+        self.grid.addWidget(self.txtAprovadorFinanceiroM,15,1)
+        self.grid.addWidget(self.txtAprovadorFinanceiroIn,15,2)
+        self.grid.addWidget(self.txtAprovadorFinanceiroAt,15,3)
+
+        self.grid.addWidget(self.lblBloqueado,16,0)
+        self.grid.addWidget(self.txtBloqueadoM,16,1)
+        self.grid.addWidget(self.txtBloqueadoIn,16,2)
+        self.grid.addWidget(self.txtBloqueadoAt,16,3)
+
+        self.grid.addWidget(self.txtCodigoDeletar,17,1)
+
+        self.grid.addWidget(self.btnDeletar,17,0)
+
+        self.grid.addWidget(self.btnSair,17,2)
  
         self.setLayout(self.grid)
         
