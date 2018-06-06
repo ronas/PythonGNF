ddd7db3a65bd4f090657f17a10813c490936e3b4
Tiago Garcia <garciax75@gmail.com>
2018-06-05 21:08:42 -0300

    aula2
    vogal consoantes

---
+++ Tiago/Aula02/L02.ExecSeq04.py
@@ -0,0 +1,12 @@
+#Faça um Programa que verifique se uma letra digitada é* **vogal ou consoante**.
+
+letra = input("Digite uma letra: ")
+
+if (letra == "a" , "e" , "i" , "o" , "u" ):
+    print ('Vogal')
+
+#if (letra == "b"):
+#    print ('Consoante')
+
+else:
+    print ('Consoante')
---
+++ Tiago/Aula02/L03.ExecSeq03.py
@@ -0,0 +1,15 @@
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M"*.
+#Conforme a letra escrever: **F - Feminino, M - Masculino, Sexo Inválido**.
+
+print ("Identificação de F para 'Feminino' e M para 'Masculino'")
+
+sexo = 'M'
+
+if sexo == 'M':
+    print ('Masculino')
+
+elif sexo == 'F':
+    print ('Feminino')
+
+else:
+    print ('Inválido')
