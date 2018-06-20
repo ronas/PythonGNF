36b04edef833ea3e4b87ae58f25d25be7f8d2d97
igor nunes de amorim <igor.nunes82@gmail.com>
2018-06-19 21:04:40 -0300

    Converção
    Converção de metro para Centimetro

---
+++ Igor/L02.Exeseq05.py
@@ -0,0 +1,13 @@
+# Autor: Igor Nunes
+# Materia: Programa Python
+# Orientador : Ronaldo
+# Data : 10/04/2018
+# Exercicio: L02.Exeseq05.py
+# Atividade: Converter metro para centimetro
+# Comentários maiores ""
+
+
+varMet=input ("Digite qtos metros quer Converter: ")
+varMet=float(varMet)
+convert=varMet*100 
+print ('Entao', varMet ,'metro(s) Equivale a' , convert , 'Centimetros')
