c3347ef3c6c9b8fbf337ce2ca4ec9b21897255ea
Tiago Garcia <garciax75@gmail.com>
2018-07-16 13:45:24 -0300

    Seq5 - aula2
    Nota Media com if

--- Tiago/Aula02/L02.ExecSeq05.py
+++ Tiago/Aula02/L02.ExecSeq05.py
@@ -4,12 +4,22 @@
 # **A mensagem "Reprovado", se a média for menor do que sete; 
 # A mensagem "Aprovado com Distinção", se a média for igual a dez**.
 
-nota1 = input('Digite a nota1: ')
-nota2 = input('Digite a nota2: ')
-
 #media >= 7 (Aprovado)
 #menor > 6 (Reprovado)
 #aprov_dist == 10 (Aprovado com Distinção)
 
-if (nota1 >=7):
-    print('Aprovado')
+
+nota1 = float (input ("Digite a nota1: "))
+nota2 = float (input ("Digite a nota2: "))
+
+nota_med = (nota1 + nota2)/2
+print (nota_med)
+
+if (nota_med >=7):
+    print ("Aprovado")
+
+if (nota_med <7):
+    print ("Reprovado")
+
+if (nota_med == 10):
+    print ("Aprovado com distinção")
